a0b4d356f7def69c6ac9e5cc3807b2fc686df955
ckeller888 <162427622+ckeller888@users.noreply.github.com>
2024-05-22 10:20:16 +0200

    feat(Uebung06): GUI-Programmieren3

---
+++ Uebung06.py
@@ -0,0 +1,30 @@
+# Uebung 6: GUI-Programmierung 3
+
+# Aufgabe 1
+
+from PyQt5.QtWidgets import *
+from PyQt5.uic import *
+from PyQt5.QtCore import *
+from sys import *
+from PyQt5.QtGui import *
+import urllib.parse 
+
+
+class Fenster(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi("Semester2/Uebungen/showmap.ui",self)
+        self.show()
+
+
+        self.Karte.clicked.connect(self.karte)
+
+    def karte(self):
+        s = f"{self.Breite.text()},{self.Laenge.text()}"    
+        link = f"https://www.google.ch/maps/place/{s}"
+        l = QDesktopServices.openUrl(QUrl(link))
+        urllib.parse.quote(l) # enthält 'Hell%C3%B6%20W%C3%B6rld%40'
+
+app = QApplication ([])
+window = Fenster()
+app.exec()
